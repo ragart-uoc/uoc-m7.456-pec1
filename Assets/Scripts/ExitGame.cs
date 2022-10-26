12eb3bab8a3791706949a71a0b85a8212057bd4e
Salvador Banderas Rovira <info@salvadorbanderas.eu>
2022-10-26 06:43:22 +0200

    Added listeners to MainMenu buttons

---
+++ Assets/Scripts/ExitGame.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class ExitGame : MonoBehaviour
+{
+    public void Exit()
+    {
+        Application.Quit();
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+    }
+}
