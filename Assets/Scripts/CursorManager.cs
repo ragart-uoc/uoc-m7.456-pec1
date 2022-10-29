ba82669c065709a392d6a442c70e1a03c8c37ead
Salvador Banderas Rovira <info@salvadorbanderas.eu>
2022-10-30 01:50:20 +0200

    Added custom cursor

---
+++ Assets/Scripts/CursorManager.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class CursorManager : MonoBehaviour
+{
+    private Texture2D _cursorTexture;
+    
+    void Start()
+    {
+        _cursorTexture = Resources.Load<Texture2D>("Sprites/cursor");
+        var cursorOffset = new Vector2(_cursorTexture.width / 2f, _cursorTexture.height / 2f);
+        Cursor.SetCursor(_cursorTexture, cursorOffset, CursorMode.Auto);
+    }
+
+}
