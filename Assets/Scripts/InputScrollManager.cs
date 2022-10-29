9fd52765240baef1527918fae9eea344e19ff5d1
Salvador Banderas Rovira <info@salvadorbanderas.eu>
2022-10-29 09:45:52 +0200

    Added HUD input UI elements and logic and reordered folders

---
+++ Assets/Scripts/InputScrollManager.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputScrollManager : MonoBehaviour
+{
+    public Button buttonUp;
+    public Button buttonDown;
+    public ScrollRect scrollRect;
+
+    private void Start()
+    {
+        buttonUp.interactable = false;
+        scrollRect.onValueChanged.AddListener(OnScrollRectValueChanged);
+    }
+    
+    private void OnScrollRectValueChanged(Vector2 value)
+    {
+        switch (value.y)
+        {
+            case > 0.9999f:
+                buttonUp.interactable = false;
+                buttonDown.interactable = true;
+                break;
+            case <= 0.0001f:
+                buttonUp.interactable = true;
+                buttonDown.interactable = false;
+                break;
+            default:
+                buttonUp.interactable = true;
+                buttonDown.interactable = true;
+                break;
+        }
+    }
+    
+    public void OnButtonUpClick()
+    {
+        if (scrollRect.normalizedPosition.y < 1)
+        {
+            scrollRect.verticalNormalizedPosition += 1 / scrollRect.scrollSensitivity;
+        }
+    }
+    
+    public void OnButtonDownClick()
+    {
+        if (scrollRect.normalizedPosition.y > 0)
+        {
+            scrollRect.verticalNormalizedPosition -= 1 / scrollRect.scrollSensitivity;
+        }
+    }
+}
