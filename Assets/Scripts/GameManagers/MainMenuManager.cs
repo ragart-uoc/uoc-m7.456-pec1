2deee7ed637134f1d7bcf9ad2431a1eaba2aa22b
Salvador Banderas Rovira <info@salvadorbanderas.eu>
2022-10-30 14:32:17 +0100

    Modified credits screen

--- Assets/Scripts/GameManagers/MainMenuManager.cs
+++ Assets/Scripts/GameManagers/MainMenuManager.cs
@@ -5,14 +5,16 @@ namespace PEC1.GameManagers
 {
     public class MainMenuManager : MonoBehaviour
     {
+        public GameObject credits;
+        
         public void StartGame()
         {
             SceneManager.LoadScene("Combat");
         }
 
-        public void ShowCredits()
+        public void ToggleCredits()
         {
-            
+            credits.SetActive(!credits.activeSelf);
         }
         
         public void QuitGame()
@@ -21,6 +23,12 @@ namespace PEC1.GameManagers
             #if UNITY_EDITOR
             UnityEditor.EditorApplication.isPlaying = false;
             #endif
+        }
+        
+        private void Update()
+        {
+            if (credits.activeSelf && Input.GetKeyUp(KeyCode.Escape))
+                ToggleCredits();
         }
     }
 }
