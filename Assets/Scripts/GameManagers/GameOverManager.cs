1ec0f34b352ad47f2edefcaea8d746c8cbdab3e9
Salvador Banderas Rovira <info@salvadorbanderas.eu>
2022-10-30 13:47:52 +0100

    Changed GameOver UI elements and behaviours

---
+++ Assets/Scripts/GameManagers/GameOverManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace PEC1.GameManagers
+{
+    public class GameOverManager : MonoBehaviour
+    {
+        public void StartGame()
+        {
+            SceneManager.LoadScene("Combat");
+        }
+
+        public void MainMenu()
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+    }
+}
